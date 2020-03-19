e8e23fbe3d280b9553699a85761953013f667d67
Hanna Sachok <anna.sachok@gmail.com>
2020-03-19 22:21:53 +0200

    JS HW1

---
+++ javascript/homework1/js/main.js
@@ -0,0 +1,20 @@
+let name = prompt('Enter your name: ');
+let age = +prompt('Enter tour age: ') || '';
+
+while (!isNaN(name)) {
+    name = prompt('Your name is uncorrected! Enter correct name:', name);
+}
+
+while (!age) {
+    age = +prompt('Uncorrected age. Please enter again: ', age);
+}
+
+if (age < 18) {
+    alert('You are not allowed to visit this website!');
+} else if (age >= 18 && age <= 22) {
+    confirm('Are you sure you want to continue?')
+        ? alert(`Welcome, ${name}`)
+        : alert('You are not allowed to visit this website');
+} else {
+    alert(`Welcome, ${name}`);
+}
