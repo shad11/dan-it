9784adc97c7b577b2980b4fa87faee6bcd5850ab
Hanna Sachok <anna.sachok@gmail.com>
2020-03-26 00:02:10 +0200

    JS HW2 Mistakes were corrected

--- javascript/homework2/js/main.js
+++ javascript/homework2/js/main.js
@@ -1,75 +1,37 @@
-// const DIVIDER = 5;
-// let number = prompt('Enter integer number: ') || null;
-// let value = 0;
-//
-// while (number === null || isNaN(number) || !Number.isInteger(+number)) {
-//     number = prompt('Entered number isn\'t corrected. Enter integer number: ', number);
-// }
-// number = +number;
-//
-// if (number < value) {
-//     console.log('Sorry, no numbers');
-// }
-//
-// while (value <= number) {
-//     console.log(value);
-//     value += DIVIDER;
-// }
+const DIVIDER = 5;
+let number = (prompt('Enter integer number: ') || '').trim();
+let value = 0;
 
-let m = parseInt(prompt('Enter the first simple integer number: '));
-let n = parseInt(prompt('Enter the second simple integer number: '));
-let errorFirst = true;
-let errorSecond = true;
+while (number === '' || !Number.isInteger(+number)) {
+    number = (prompt('Entered number isn\'t corrected. Enter integer number: ') || '').trim();
+}
+number = +number;
 
-while (true) {
-    if (!m || m < 2) {
-        errorFirst = true;
-    } else if (errorFirst) {
-        errorFirst = false;
-        for (let i = 2; i < m; i++) {
-            if (m % i === 0) {
-                errorFirst = true;
-            }
-        }
-    }
-
-    if (!n || n < 2) {
-        errorSecond = true;
-    } else if (errorSecond) {
-        errorSecond = false;
-        for (let i = 2; i < n; i++) {
-            if (n % i === 0) {
-                errorSecond = true;
-            }
-        }
-    }
-
-    if (errorFirst) {
-        m = parseInt(prompt('Entered number isn\'t corrected. Enter the first simple integer number: ', m));
-    } else if (errorSecond) {
-        n = parseInt(prompt('Entered number isn\'t corrected. Enter the second simple integer number: ', n));
-    } else {
-        break;
+if (number < value) {
+    console.log('Sorry, no numbers');
+} else {
+    while (value <= number) {
+        console.log(value);
+        value += DIVIDER;
     }
 }
-/*
-let startNumber = m;
 
-while (startNumber <= n) {
-    let i = 2;
+// additional task
 
-    for (;i < startNumber; i++) {
-        if (startNumber % i === 0) {
-            break;
-        }
-    }
+let num1 = (prompt('Enter the first simple integer number: ') || '').trim();
+let num2 = (prompt('Enter the second simple integer number: ') || '').trim();
 
-    if (i === startNumber) {
-        console.log(startNumber);
-    }
+while (num1 === '' || num2 === '' || !Number.isInteger(+num1) || !Number.isInteger(+num2)) {
+    num1 = (prompt('Enter the first simple integer number: ') || '').trim();
+    num2 = (prompt('Enter the second simple integer number: ') || '').trim();
+}
 
-    startNumber++;
-}*/
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
+let m = num1 < num2 ? num1 : num2;
+let n = num1 > num2 ? num1 : num2;
+
 simpleNumber:
 for (let number = m; number <= n; number++) {
     for (let i = 2; i < number; i++) {
