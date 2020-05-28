542d12540b2d1a3eccce88d3ab0bdf1abf375f53
Hanna Sachok <anna.sachok@gmail.com>
2020-05-28 17:49:14 +0300

    JS HW9 Optimization

--- javascript/homework9/js/main.js
+++ javascript/homework9/js/main.js
@@ -1,18 +1,21 @@
 const ready = () => {
     let prevLi = document.querySelector('.tabs > li.active');
 
-    document.querySelectorAll('.tabs > li').forEach((elem) => {
-        elem.addEventListener('click', () => {
+    document.querySelectorAll('.tabs > li').forEach((elemLi) => {
+        elemLi.addEventListener('click', (event) => {
+            const elem = event.target;
             const tabName = elem.dataset.tab;
+            const currTab = document.querySelector(`#${tabName}`);
 
             elem.classList.add('active');
-            document.querySelector(`#${tabName}`).classList.add('active');
+            currTab.classList.add('active');
 
             if (prevLi && prevLi !== elem) {
                 const prevTabName = prevLi.dataset.tab;
+                const prevTab = document.querySelector(`#${prevTabName}`);
 
                 prevLi.classList.remove('active');
-                document.querySelector(`#${prevTabName}`).classList.remove('active');
+                prevTab.classList.remove('active');
 
                 prevLi = elem;
             }
