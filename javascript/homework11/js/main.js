4e2bed8f1cbb656530b3c8ec21b21aa7c000519e
Hanna Sachok <anna.sachok@gmail.com>
2020-06-06 23:52:39 +0300

    JS HW11 Correcting according notes

--- javascript/homework11/js/main.js
+++ javascript/homework11/js/main.js
@@ -1,29 +1,24 @@
 let prevBtnEl;
-let listBtn = [];
 
 const paintBtn = keyCode => {
+    const letter = keyCode.indexOf('Key') >= 0 ? keyCode.slice(3) : keyCode;
+
+    const currBtnEl = Array.from(document.querySelectorAll('.btn-wrapper .btn'))
+        .find( elem => elem.innerText === letter);
+
     if (prevBtnEl) {
         prevBtnEl.style.backgroundColor = '#33333a';
     }
 
-    if (!listBtn.includes(keyCode)) {
+    if (!currBtnEl) {
         return;
     }
-
-    const currBtnEl = document.querySelector(`[data-key-code='${keyCode}']`);
 
     currBtnEl.style.backgroundColor = 'blue';
     prevBtnEl = currBtnEl;
 };
 
 const onReady = () => {
-    document.querySelectorAll('.btn-wrapper .btn').forEach(elem => {
-        const keyCode = elem.innerText.length > 1 ? elem.innerText : 'Key' + elem.innerText.toUpperCase();
-
-        elem.dataset.keyCode = keyCode;
-        listBtn.push(keyCode);
-    });
-
     document.addEventListener('keyup', event => {
         paintBtn(event.code);
     });
