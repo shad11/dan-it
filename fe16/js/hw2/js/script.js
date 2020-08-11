0a58da1b61fc02ec38e930b25e92df7e9c272486
Hanna Sachok <anna.sachok@gmail.com>
2020-08-11 19:00:29 +0300

    FE16 JS2

---
+++ fe16/js/hw2/js/script.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: "Скотт Бэккер",
+        name: "Тьма, что приходит прежде",
+        price: 70
+    },
+    {
+        author: "Скотт Бэккер",
+        name: "Воин-пророк",
+    },
+    {
+        name: "Тысячекратная мысль",
+        price: 70
+    },
+    {
+        author: "Скотт Бэккер",
+        name: "Нечестивый Консульт",
+        price: 70
+    },
+    {
+        author: "Дарья Донцова",
+        name: "Детектив на диете",
+        price: 40
+    },
+    {
+        author: "Дарья Донцова",
+        name: "Дед Снегур и Морозочка",
+    }
+];
+
+const CHECK_PROPERTIES = ['name', 'author', 'price'];
+
+const checkProperties = (object) => {
+    for (const prop of CHECK_PROPERTIES) {
+        if (!(prop in object)) {
+            throw new Error(`A property ${prop} doesn't exist!`);
+        }
+    }
+
+    return true;
+};
+
+const getBooks = () => {
+    return books.filter(elem => {
+        try {
+            return checkProperties(elem);
+        } catch (error) {
+            console.log(error.message);
+        }
+    });
+};
+
+const createBookUl = (books) => {
+    const elemUl = document.createElement('ul');
+
+    books.forEach(book => {
+        const elemLi = document.createElement('li');
+
+        CHECK_PROPERTIES.forEach(prop => {
+            elemLi.innerHTML += `${prop}: ${book[prop]}; `;
+        });
+        elemLi.innerHTML = elemLi.innerHTML.charAt(0).toUpperCase() + elemLi.innerHTML.slice(1);
+
+        elemUl.append(elemLi);
+    });
+
+    return elemUl;
+};
+
+const showBooks = () => {
+    const booksArray = getBooks();
+    const booksUl = createBookUl(booksArray);
+    const elemRoot = document.querySelector('#root');
+
+    elemRoot.append(booksUl);
+};
+
+const onReady = () => {
+    showBooks();
+};
+
+document.addEventListener('DOMContentLoaded', onReady);
