a1d47df28caa2536c3d92b52b849c4f86ed87e7c
Hanna Sachok <anna.sachok@gmail.com>
2020-11-18 17:49:29 +0200

    FE16 React5 Refactoring

--- fe16/react/hw5/src/pages/Cart/Cart.js
+++ fe16/react/hw5/src/pages/Cart/Cart.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import Loader from "../../components/Loader";
 import Products from "../../components/Products";
-import OrderForm from "../../components/Forms/OrderForm";
+import { OrderForm } from "../../components/Forms";
 import PropTypes from "prop-types";
 import "./Cart.scss";
 
--- fe16/react/hw5/src/pages/Header/Header.js
+++ fe16/react/hw5/src/pages/Header/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import MainMenu from "../../components/MainMenu";
-import AddMenu from "../../components/AddMenu";
+import { MainMenu, AddMenu } from "../../components/Menu";
 import './Header.scss';
 
 const Header = (props) => (
